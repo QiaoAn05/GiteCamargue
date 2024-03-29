f563c4d20a093ef0c8f61b0c004b2ca5fca6dd0b
Johan <johan.perez05@gmail.com>
2024-03-29 11:17:13 +0100

    add the sections and hero banner without img

--- assets/react/controllers/Home.jsx
+++ assets/react/controllers/Home.jsx
@@ -1,14 +1,58 @@
 import React from 'react';
+import accueil from "../../images/photos-accueil-mas.jpg";
 
 export default function () {
     
     return (
     
-    <div className='text-dark my-5'>
-        <div className="d-grid gap-2 d-md-flex justify-content-md-center">
-            <button className="btn btn-primary me-md-2" type="button">Button</button>
-            <button className="btn btn-dark" type="button">Button</button>
+    <div className='text-dark mb-5'>
+        <div className="hero-banner bg-dark opacity-50 d-flex align-items-center">
+            <div className="container text-white">
+                <p className='text-center'>Venez découvrir la Camargue au</p>
+                <h1 className="text-center">Mas de Guinot</h1>
+            </div>
         </div>
+
+        <section className="py-5 bg-light">
+            <div className="container">
+                <div className="row">
+                    <div className="col-md-6">
+                        <div className="d-flex flex-column justify-content-center h-100">
+                            <h2 className="text-center mb-5 text-dark">Bienvenue au Mas de Guinot</h2>
+                            <p className="mb-5 mx-5">
+                            La famille Crozat vous accueillent au Mas de Guinot , domaine rizicole de 80 hectares situé au cœur du parc régional de Camargue entre Arles et Salin de Giraud.
+                            </p>
+                            <button className="btn btn-dark text-white mx-auto">A propos</button>
+                        </div>
+                    </div>
+                    <div className="col-md-6 text-center">
+                        <img src={accueil} alt="Description de l'image" className="img-fluid" />
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <section className="py-5 bg-dark text-white">
+            <div className="container">
+                <div className="row">
+                    <div className="col-md-6 text-center">
+                        <img src={accueil} alt="Description de l'image" className="img-fluid" />
+                    </div>
+                    <div className="col-md-6">
+                        <div className="d-flex flex-column justify-content-center h-100">
+                            <h2 className="text-center mb-5">Gîtes en location</h2>
+                            <p className="mb-5 mx-5">
+                            Venez découvrir nos gîtes en plein coeur d'une Camargue resplendissante !</p>
+                            <div className="d-flex justify-content-center">
+                                <button className="btn btn-light mx-2">Nos gîtes</button>
+                                <button className="btn btn-light mx-2">Nos tarifs</button>
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
     </div>
     
     );
