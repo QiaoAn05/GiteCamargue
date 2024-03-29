0a5e73061d4de36f9b613de7c0199530eb0c72e4
Johan <johan.perez05@gmail.com>
2024-03-29 08:47:27 +0100

    header bootstrap done

--- assets/react/controllers/Header.jsx
+++ assets/react/controllers/Header.jsx
@@ -4,13 +4,13 @@ export default function () {
     
     return (
         
-        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
         <div className="container-fluid">
           <a className="navbar-brand" href="#">LE MAS GUINOT</a>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
-          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <div className="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
             <ul className="navbar-nav mb-lg-0">
               <li className="nav-item">
                 <a className="nav-link active" aria-current="page" href="/">Accueil</a>
