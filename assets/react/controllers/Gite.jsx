6af83fbe2930d0fcea122fef04a45b2fac5591d4
Johan <johan.perez05@gmail.com>
2024-04-05 18:28:50 +0200

    add cards into the gite page

--- assets/react/controllers/Gite.jsx
+++ assets/react/controllers/Gite.jsx
@@ -1,9 +1,41 @@
 import React from 'react';
-// import accueil from "../../images/photos-accueil-mas.jpg";
 
-export default function () {
-    
-    return <>
-        <div>Hello test Gite</div>
-    </>
+export default function Home() {
+    return (
+        <>
+            <div className="hero-banner d-flex">
+                <div className="container-fluid text-white text-center">
+                    <h1>Nos Gites</h1>
+                </div>
+            </div>
+            <section className='container py-5'>
+                <div className='row'>
+                    <div className='col-md-4'>
+                        <div className='card'>
+                            <div className='card-body'>
+                                <h5 className='card-title'>Gite 1</h5>
+                                <p className='card-text'>Contenu gite 1</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className='col-md-4'>
+                        <div className='card'>
+                            <div className='card-body'>
+                                <h5 className='card-title'>Gite 2</h5>
+                                <p className='card-text'>Contenu gite 2</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className='col-md-4'>
+                        <div className='card'>
+                            <div className='card-body'>
+                                <h5 className='card-title'>Gite 3</h5>
+                                <p className='card-text'>Contenu gite 3</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </>
+    );
 }
