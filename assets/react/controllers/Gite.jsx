7b06a0915f5eff5788ccaffdd8678eaf613a73d3
Johan <johan.perez05@gmail.com>
2024-04-11 18:53:04 +0200

    add the method show with serializer to send a json response and use axios to get the response data to display

--- assets/react/controllers/Gite.jsx
+++ assets/react/controllers/Gite.jsx
@@ -1,9 +1,23 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import Mazet from '../../images/gite-mazet.jpg';
 import Pigeonnier from '../../images/gite-pigeonnier.jpg';
 import Bergerie from '../../images/gite-bergerie.jpg';
 
-export default function () {
+export default function Gites() {
+    const [gites, setGites] = useState([]);
+
+    useEffect(() => {
+        axios.get('api/gites')
+            .then(response => {
+                setGites(response.data);
+                // console.log("data :", response.data);
+            })
+            .catch(error => {
+                console.error('Error fetching gites:', error);
+            });
+    }, []);
+
     return (
         <>
             <div className="hero-banner d-flex">
@@ -16,59 +30,28 @@ export default function () {
                 <div className='row'>
                     <div className="d-flex mb-3">
                         <button className='btn btn-dark'>Ajouter un gite</button>
-                    </div>   
+                    </div>
                 </div>
 
                 <div className='row'>
-                    <div className='col-12 col-lg-4 mb-4'>
-                        <div className='card'>
-                            <div className='card-body'>
-                                <img src={ Mazet } alt="photo du Mazet" className='card-img' />
-                                <h5 className='card-title'>Le Mazet</h5>
-                                <p className='card-text'>Contenu gite 1</p>
-                                <div className="d-flex justify-content-between">
-                                    <a href="/tarifs" className='text-dark'>Voir le tarif</a>
-                                    <div className="d-flex align-items-center">
-                                        <i className="bi bi-pencil-square text-success  me-2"></i>
-                                        <i className="bi bi-trash text-danger"></i>
-                                    </div>
-                                </div>
-                                
-                            </div>
-                        </div>
-                    </div>
-                    <div className='col-12 col-lg-4 mb-4'>
-                        <div className='card'>
-                            <div className='card-body'>
-                                <img src={ Pigeonnier } alt="photo du Pigeonnier" className='card-img' />
-                                <h5 className='card-title'>Le Pigeonnier</h5>
-                                <p className='card-text'>Contenu gite 2</p>
-                                <div className="d-flex justify-content-between">
-                                    <a href="/tarifs" className='text-dark'>Voir le tarif</a>
-                                    <div className="d-flex align-items-center">
-                                        <i className="bi bi-pencil-square text-success  me-2"></i>
-                                        <i className="bi bi-trash text-danger"></i>
+                    {gites.map(gite => (
+                        <div key={gite.id} className='col-12 col-lg-4 mb-4'>
+                            <div className='card'>
+                                <div className='card-body'>
+                                    <img src={gite.image} alt={`photo du ${gite.name}`} className='card-img' />
+                                    <h5 className='card-title'>{gite.name}</h5>
+                                    <p className='card-text'>{gite.description}</p>
+                                    <div className="d-flex justify-content-between">
+                                        <a href="/tarifs" className='text-dark'>Voir le tarif</a>
+                                        <div className="d-flex align-items-center">
+                                            <i className="bi bi-pencil-square text-success  me-2"></i>
+                                            <i className="bi bi-trash text-danger"></i>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
                         </div>
-                    </div>
-                    <div className='col-12 col-lg-4 mb-4'>
-                        <div className='card'>
-                            <div className='card-body'>
-                                <img src={ Bergerie } alt="photo de la Bergerie" className='card-img' />
-                                <h5 className='card-title'>La Bergerie</h5>
-                                <p className='card-text'>Contenu gite 3</p>
-                                <div className="d-flex justify-content-between">
-                                    <a href="/tarifs" className='text-dark'>Voir le tarif</a>
-                                    <div className="d-flex align-items-center">
-                                        <i className="bi bi-pencil-square text-success  me-2"></i>
-                                        <i className="bi bi-trash text-danger"></i>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+                    ))}
                 </div>
             </section>
         </>
--- src/Controller/GiteController.php
+++ src/Controller/GiteController.php
@@ -4,21 +4,29 @@ namespace App\Controller;
 
 use App\Repository\GiteRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Serializer\SerializerInterface;
 
 class GiteController extends AbstractController
 {
     #[Route('/gite', name: 'app_gite')]
-    public function index(Request $request, GiteRepository $giteRepository): Response
+    public function index(): Response
     {
 
-        $gites = $giteRepository -> findAll();
-        // dd($gites);
-
-        return $this->render('gite/gite.html.twig', [
-            'gites' => $gites
-        ]);
+        return $this->render('gite/gite.html.twig');
     }
+
+    #[Route('api/gites', name: 'api_gites', methods: 'GET')]
+    public function show(GiteRepository $giteRepository, SerializerInterface $serializer): Response
+    {
+        $gites = $giteRepository->findAll();
+        
+        $data = $serializer->serialize($gites, 'json');
+    
+        return new JsonResponse($data, Response::HTTP_OK, [], true);
+    }
+
+
 }
