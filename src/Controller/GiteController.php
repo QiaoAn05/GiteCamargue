867cddb8ea34709388cf6c67b13448afcd1cfaa5
Johan <johan.perez05@gmail.com>
2024-04-11 17:47:36 +0200

    add the logic to read the database into the giteController

--- src/Controller/GiteController.php
+++ src/Controller/GiteController.php
@@ -2,17 +2,23 @@
 
 namespace App\Controller;
 
+use App\Repository\GiteRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class GiteController extends AbstractController
 {
     #[Route('/gite', name: 'app_gite')]
-    public function index(): Response
+    public function index(Request $request, GiteRepository $giteRepository): Response
     {
+
+        $gites = $giteRepository -> findAll();
+        // dd($gites);
+
         return $this->render('gite/gite.html.twig', [
-            'controller_name' => 'GiteController',
+            'gites' => $gites
         ]);
     }
 }
