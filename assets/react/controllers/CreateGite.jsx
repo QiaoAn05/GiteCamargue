78a51f9f3a9d67fa7447f1a7120f765753af80cf
Johan <johan.perez05@gmail.com>
2024-04-11 21:45:08 +0200

    add react-bootstrap to add the accordion component CreateGite

---
+++ assets/react/controllers/CreateGite.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import Accordion from 'react-bootstrap/Accordion';
+
+function CreateGite() {
+  return (
+    <Accordion className='mb-3 dark'>
+      <Accordion.Item eventKey="0">
+        <Accordion.Header><b>Ajouter un gîte</b></Accordion.Header>
+        <Accordion.Body>
+            <form>
+                <div className="mb-3">
+                    <label htmlFor="giteName" className="form-label">Nom du gîte</label>
+                    <input type="text" className="form-control" id="giteName" />
+                </div>
+                <div className="mb-3">
+                    <label htmlFor="giteDescription" className="form-label">Description</label>
+                    <textarea className="form-control" id="giteDescription" rows="3"></textarea>
+                </div>
+                <div className="mb-3">
+                    <label htmlFor="giteImage" className="form-label">Image</label>
+                    <input type="file" className="form-control" id="giteImage" />
+                </div>
+                <button type="submit" className="btn btn-primary">Enregistrer</button>
+            </form>
+        </Accordion.Body>
+      </Accordion.Item>
+    </Accordion>
+  );
+}
+
+export default CreateGite;
--- assets/react/controllers/Gite.jsx
+++ assets/react/controllers/Gite.jsx
@@ -3,6 +3,7 @@ import axios from 'axios';
 import Mazet from '../../images/gite-mazet.jpg';
 import Pigeonnier from '../../images/gite-pigeonnier.jpg';
 import Bergerie from '../../images/gite-bergerie.jpg';
+import CreateGite from './CreateGite';
 
 export default function Gites() {
     const [gites, setGites] = useState([]);
@@ -26,12 +27,12 @@ export default function Gites() {
                 </div>
             </div>
             <section className='container py-5'>
-
-                <div className='row'>
+                <CreateGite/>
+                {/* <div className='row'>
                     <div className="d-flex mb-3">
-                        <button className='btn btn-dark'>Ajouter un gite</button>
+                        <button type='button' className='btn btn-dark' data-bs-toggle="modal" data-bs-target="#exampleModal">Ajouter un gite</button>
                     </div>
-                </div>
+                </div> */}
 
                 <div className='row'>
                     {gites.map(gite => (
@@ -54,6 +55,25 @@ export default function Gites() {
                     ))}
                 </div>
             </section>
+
+            {/* <div className="modal fade" id="exampleModal" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div className="modal-dialog">
+                    <div className="modal-content">
+                    <div className="modal-header">
+                        <h5 className="modal-title" id="exampleModalLabel">Modal title</h5>
+                        <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div className="modal-body">
+                        ...
+                    </div>
+                    <div className="modal-footer">
+                        <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                        <button type="button" className="btn btn-primary">Save changes</button>
+                    </div>
+                    </div>
+                </div>
+            </div> */}
+            
         </>
     );
 }
